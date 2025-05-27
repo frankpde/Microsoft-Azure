ef643a4408b9d2139a2bea6ee3fa88e2fa912937
pwweb <clients@pw-websolutions.com>
2025-05-27 21:36:48 +0200

    obtain Azure avatar post authorization

--- User.php
+++ User.php
@@ -3,6 +3,7 @@
 namespace SocialiteProviders\Azure;
 
 use SocialiteProviders\Manager\OAuth2\User as oAuth2User;
+use Illuminate\Support\Facades\Http;
 
 class User extends oAuth2User
 {
@@ -38,5 +39,22 @@ class User extends oAuth2User
     public function getMail()
     {
         return $this->mail;
+    }
+
+    /**
+     * Get the avatar for the user.
+     *
+     * @return string
+     */
+    public function getAvatar(): ?string
+    {
+        $response = Http::withToken($this->token)
+            ->get('https://graph.microsoft.com/v1.0/me/photo/$value');
+
+        if ($response->successful()) {
+            return $response->body();
+        }
+
+        return null;
     }
 }
